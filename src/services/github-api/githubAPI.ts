35253c8c0b720704b8e37489265e4c856c744584
ricardolsousa <ricardolsousa2909@gmail.com>
2025-01-22 16:02:07 +0000

    feat: added service to make calls to github API

---
+++ src/services/github-api/githubAPI.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const githubAPI = axios.create({
+  baseURL: "https://api.github.com",
+  headers: {
+    Accept: "application/vnd.github.v3+json",
+  },
+});
+
+export const getUser = async (username: string) => {
+  const response = await githubAPI.get(`/users/${username}`);
+  return response.data;
+};
