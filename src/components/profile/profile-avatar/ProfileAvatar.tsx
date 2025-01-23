6a95d9e30d6e623a4aa472f93bddd35c610b2c11
ricardolsousa <ricardolsousa2909@gmail.com>
2025-01-23 23:54:45 +0000

    feat: profile avatar component added

---
+++ src/components/profile/profile-avatar/ProfileAvatar.tsx
@@ -0,0 +1,28 @@
+import { BsGithub } from "react-icons/bs";
+import { Link } from "react-router";
+
+type ProfileAvatarProps = {
+  profile: any;
+};
+
+const ProfileAvatar = ({ profile }: ProfileAvatarProps) => {
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <div>
+        <img
+          src={profile?.avatar_url}
+          width={150}
+          height={150}
+          alt=""
+          className="rounded-full"
+        />
+      </div>
+      <Link to={profile?.html_url} className="flex gap-1 items-center">
+        <BsGithub />
+        <h1>{profile?.login}</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default ProfileAvatar;
