c9289bcacb99e850eab327c7461084bf6be5e33c
ricardolsousa <ricardolsousa2909@gmail.com>
2025-01-23 19:07:36 +0000

    fix: fixed profile details ui

--- src/components/profile/profile-details/ProfileDetails.tsx
+++ src/components/profile/profile-details/ProfileDetails.tsx
@@ -6,20 +6,20 @@ const ProfileDetails = ({ profile }: ProfileDetailsProps) => {
   return (
     <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-1 gap-4">
       <div className="bg-gray-200 rounded px-8 py-1 flex flex-col items-center justify-center">
-        <div>{profile.followers}</div>
-        <div>Followers</div>
+        <strong>{profile.followers}</strong>
+        <div className="text-sm">Followers</div>
       </div>
       <div className="bg-gray-200 rounded px-8 py-1 flex flex-col items-center justify-center">
-        <div>{profile.following}</div>
-        <div>Following</div>
+        <strong>{profile.following}</strong>
+        <div className="text-sm">Following</div>
       </div>
       <div className="bg-gray-200 rounded px-8 py-1 flex flex-col items-center justify-center">
-        <div>{profile.public_repos}</div>
-        <div>Repositories</div>
+        <strong>{profile.public_repos}</strong>
+        <div className="text-sm">Repositories</div>
       </div>
       <div className="bg-gray-200 rounded px-8 py-1 flex flex-col items-center justify-center">
-        <div>{profile.public_gists}</div>
-        <div>Gists</div>
+        <strong>{profile.public_gists}</strong>
+        <div className="text-sm">Gists</div>
       </div>
     </div>
   );
