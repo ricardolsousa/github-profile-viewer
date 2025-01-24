9b16344a03e67f7742869361f5a7223a7f22f675
ricardolsousa <ricardolsousa2909@gmail.com>
2025-01-24 01:57:35 +0000

    feat: added new design to homepage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -10,13 +10,24 @@ const HomePage = () => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <button onClick={async () => await handleGetUser()}>Search</button>
+    <div className="lg:mx-32 md:mx-1 sm:mx-1 xs:mx-1 lg:px-6 px-4 py-8 flex flex-col gap-4 justify-center items-center">
+      <strong className="text-3xl">GitHub Profile Viewer</strong>
+      <div className="mt-6 flex items-center">
+        <input
+          type="text"
+          style={{ minWidth: "256px" }}
+          className="rounded border border-gray-200 py-2 px-3 bg-gray-600 text-base bg-white mr-6"
+          placeholder="Enter GitHub username..."
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button
+          onClick={async () => await handleGetUser()}
+          className="inline-flex w-full justify-center rounded bg-blue-600 py-2 px-3 text-base font-semibold text-white shadow-sm"
+        >
+          Search
+        </button>
+      </div>
     </div>
   );
 };
