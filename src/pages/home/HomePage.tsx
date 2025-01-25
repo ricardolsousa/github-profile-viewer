f138a48a32809fb7a97623428169e7ad783ca08a
ricardolsousa <ricardolsousa2909@gmail.com>
2025-01-25 02:27:03 +0000

    feat: added text input into homepage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import "./styles.css";
+import TextInput from "../../components/utils/text-input/TextInput";
 
 const HomePage = () => {
   const [username, setUsername] = useState<string>("");
@@ -14,10 +15,9 @@ const HomePage = () => {
     <div className="lg:mx-32 md:mx-1 sm:mx-1 xs:mx-1 lg:px-6 px-4 py-8 flex flex-col gap-4 justify-center items-center">
       <strong className="text-3xl text-center">GitHub Profile Viewer</strong>
       <div className="mt-6 flex items-center search-container">
-        <input
-          type="text"
-          style={{ minWidth: "256px" }}
-          className="rounded border border-gray-200 py-2 px-3 bg-gray-600 text-base bg-white mr-6"
+        <TextInput
+          styles={{ minWidth: "256px" }}
+          classes="rounded border border-gray-200 py-2 px-3 bg-gray-600 text-base bg-white mr-6"
           placeholder="Enter GitHub username..."
           value={username}
           onChange={(e) => setUsername(e.target.value)}
